e0511074ba82716b07fb66fdf79828e46f1189cb
Michael Goffioul <michael.goffioul@gmail.com>
2023-10-15 20:43:38 -0400

    Allow soundtrigger HAL to load lib from vndk APEX
    The default soundtrigger HAL needs access to
    /apex/com.android.vndk.v${VNDK}/${LIB}/android.hardware.soundtrigger@2.0.so
    /apex/com.android.vndk.v${VNDK}/${LIB}/android.hardware.soundtrigger@2.0-core.so
    
    It's unclear why those 2 in particular do not end up on /system/${LIB}.
    This fix might not be needed anymore once the build is Treble-ized.
    
    Change-Id: I2325f61d44de11dc2a3289c7e46b31caff730850

--- contents/namespace/systemdefault.cc
+++ contents/namespace/systemdefault.cc
@@ -104,6 +104,7 @@ Namespace BuildSystemDefaultNamespace([[maybe_unused]] const Context& ctx) {
     ns.AddSearchPath("/system/vendor/${LIB}/egl");
     ns.AddSearchPath("/vendor/${LIB}/egl");
     ns.AddSearchPath("/vendor/${LIB}/hw");
+    ns.AddSearchPath("/apex/com.android.vndk.v" + Var("VENDOR_VNDK_VERSION") + "/${LIB}");
     ns.AddSearchPath("/odm/${LIB}");
   }
 
